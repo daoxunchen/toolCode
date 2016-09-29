09b78cd18fa9fc7f4e87fe6fec55496d812e13da
daoxunchen <acy631545607@gmail.com>
2016-09-29 13:30:45 +0800

    default not getMem

--- Timing.h
+++ Timing.h
@@ -1,6 +1,7 @@
 #ifndef TIMING_H
 
 #include <cstdio>
+#include <cstdint>
 
 #if defined(_MSC_VER) || defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(WIN64) || defined(_WIN64) || defined(__WIN64__)
 #define TIMING_WIN32
@@ -8,12 +9,33 @@
 
 #define timing_t double
 
+void printInfo(timing_t times, uint64_t mem)
+{
+	printf("--->Elapsed Timing(ms): %.3f\n", times);
+	if (mem)
+		printf("--->Memory  Usage(KB) : %llu\n", mem);
+	printf("----------------------------------------\n");
+}
+
 #ifdef TIMING_WIN32
 
 #include <windows.h>
 #include <Psapi.h>
 
 #pragma comment(lib, "Psapi.lib")
+
+SIZE_T memoryUsed()
+{
+	SIZE_T res = 0;
+	if (HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, false, GetCurrentProcessId())) {
+		PROCESS_MEMORY_COUNTERS pmc;
+		if (GetProcessMemoryInfo(hProcess, &pmc, sizeof(pmc))) {
+			res = pmc.PeakWorkingSetSize >> 10;
+		}
+		CloseHandle(hProcess);
+	}
+	return res;
+}
 
 LARGE_INTEGER time_start, time_over;
 LONGLONG dqFreq;
@@ -34,31 +56,18 @@ inline timing_t stopTiming()
 	return ((time_over.QuadPart - time_start.QuadPart) * 1000 / static_cast<timing_t>(dqFreq));
 }
 
-inline timing_t stopWithPrintTiming()
+inline timing_t stopWithPrintTiming(bool getMem = false)
 {
 	QueryPerformanceCounter(&time_over);
 	timing_t timing = ((time_over.QuadPart - time_start.QuadPart) * 1000 / static_cast<timing_t>(dqFreq));
-	printf("--->Elapsed Timing(ms): %.3f\n", timing);
-
-	if (HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, false, GetCurrentProcessId())) {
-		PROCESS_MEMORY_COUNTERS pmc;
-		if (GetProcessMemoryInfo(hProcess, &pmc, sizeof(pmc))) {
-			printf("--->Memory  Usage(KB) : %llu\n", pmc.PeakWorkingSetSize >> 10);
-		}
-		CloseHandle(hProcess);
-	}
-	printf("----------------------------------------\n");
-	system("pause");
+	printInfo(timing, getMem ? memoryUsed() : 0);
 	return timing;
 }
 
 #else //	Other OS
 
-#include <cstdint>
 #include <time.h>
 #include <unistd.h>
-
-#include <iostream>
 
 timespec time_start, time_end;
 
@@ -79,15 +88,14 @@ inline timing_t stopWithPrintTiming()
 	clock_gettime(CLOCK_MONOTONIC, &time_end);
 
 	timing_t timing = (time_end.tv_sec - time_start.tv_sec) * 1000 + (time_end.tv_nsec - time_start.tv_nsec) / 1000000.0;
-	printf("--->Elapsed Timing(ms) : %.3f\n", timing);
-	printf("----------------------------------------\n");
+	printInfo(timing, 0);
 	return timing;
 }
 
 #endif // end of ifdef TIMING_WIN32
 
 // unit: ms
-inline void wait(int ms)
+inline void wait(size_t ms)
 {
 #ifdef TIMING_WIN32
 	Sleep(ms);
